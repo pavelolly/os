62dbfb1a2d67925917512a2beae90fea447b68fe
km <lvanfomin069@gmail.com>
2024-04-01 23:20:25 +0300

    make chat with random message reciever

--- semaphore/semaphore.cpp
+++ semaphore/semaphore.cpp
@@ -4,26 +4,43 @@
 #include <Windows.h>
 
 
-HANDLE semaphore;
+HANDLE semaphore1;
+HANDLE semaphore2;
 
 void Write() {
     char buffer[256];
+    std::ofstream out;
     while (true) {
+        std::cout << " >> ";
         std::cin >> buffer;
-        std::cout << " > got input: " << buffer << std::endl;
-        ReleaseSemaphore(semaphore, 1, NULL);
+
+        out.open("msg");
+
+        out << buffer << std::endl;
+
+        out.close();
+
+        ReleaseSemaphore(semaphore1, 1, NULL);
     }
 }
 
 void Read() {
+    char buffer[256] = { 0 };
+    std::ifstream in;
     while (true) {
-        WaitForSingleObject(semaphore, INFINITE);
-        std::cout << " > fetched data from file" << std::endl;
+        WaitForSingleObject(semaphore1, INFINITE);
+
+        in.open("msg");
+
+        in >> buffer;
+        std::cout << "\n > got message: " << buffer << "\n >> ";
+
+        in.close();
     }
 }
 
 int main() {
-    semaphore = CreateSemaphore(NULL, 0, 1, L"Semaphore");
+    semaphore1 = CreateSemaphore(NULL, 0, 1, L"Semaphore");
     std::thread WriteThread(Write);
     std::thread ReadThread(Read);
 

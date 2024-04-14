88bec6c3466b7d9d341edb97981e2bd0874a8987
km <lvanfomin069@gmail.com>
2024-04-14 12:35:44 +0300

    add timers for mutex project

--- mutex/mutex.cpp
+++ mutex/mutex.cpp
@@ -3,6 +3,7 @@
 #include <mutex>
 #include <atomic>
 #include <vector>
+#include <ctime>
 
 
 void doCalculations(int& n) {
@@ -23,7 +24,7 @@ void doCalculationsAtomic(std::atomic<int>& n) {
 }
 
 
-#define CALCULATIONS_COUNT 10000
+#define CALCULATIONS_COUNT 100000
 template<typename T>
 void doCalculationsMany(void (*func)(T&), T& arg) {
 	for (int i = 0; i < CALCULATIONS_COUNT; ++i) {
@@ -39,6 +40,9 @@ int main() {
 	std::cout << "number of calculations per thread: " << CALCULATIONS_COUNT << "\n";
 	std::cout << "expected value: " << THREADS_COUNT * CALCULATIONS_COUNT << "\n";
 
+	clock_t t1, t2;
+	
+	t1 = clock();
 	int n = 0;
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i] = std::thread(doCalculationsMany<int>, doCalculations, std::ref(n));
@@ -46,9 +50,12 @@ int main() {
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i].join();
 	}
+	t2 = clock();
 
 	std::cout << "No mutex, no atomic calculations result: " << n << "\n";
+	std::cout << "Time: " << (float)(t2 - t1) / CLOCKS_PER_SEC << "s\n\n";
 
+	t1 = clock();
 	n = 0;
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i] = std::thread(doCalculationsMany<int>, doCalculationsMutex, std::ref(n));
@@ -56,9 +63,12 @@ int main() {
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i].join();
 	}
+	t2 = clock();
 
 	std::cout << "Mutex calculations result: " << n << "\n";
+	std::cout << "Time: " << (float)(t2 - t1) / CLOCKS_PER_SEC << "s\n\n";
 
+	t1 = clock();
 	std::atomic<int> an = 0;
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i] = std::thread(doCalculationsMany<std::atomic<int>>, doCalculationsAtomic, std::ref(an));
@@ -66,8 +76,10 @@ int main() {
 	for (int i = 0; i < THREADS_COUNT; ++i) {
 		threads[i].join();
 	}
+	t2 = clock();
 	
 	std::cout << "Atomic calculations result: " << an << "\n";
+	std::cout << "Time: " << (float)(t2 - t1) / CLOCKS_PER_SEC << "s\n\n";
 
 	return 0;
 }

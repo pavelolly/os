b9a05ca7657111c9165ac2e585b739f9648ac3bc
km <lvanfomin069@gmail.com>
2024-05-01 18:10:54 +0300

    semaphore: tow users can now talk via chat!

--- semaphore/semaphore.cpp
+++ semaphore/semaphore.cpp
@@ -1,48 +1,62 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <thread>
+#include <assert.h>
 #include <Windows.h>
 
 
 HANDLE semaphore1;
 HANDLE semaphore2;
 
-void Write() {
+void Write(int proccess_number) {
     char buffer[256];
     std::ofstream out;
     while (true) {
         std::cout << " >> ";
-        std::cin >> buffer;
+        std::cin.getline(buffer, 256);
 
         out.open("msg");
-
         out << buffer << std::endl;
-
         out.close();
 
-        ReleaseSemaphore(semaphore1, 1, NULL);
+        if (proccess_number == 1) {
+            ReleaseSemaphore(semaphore1, 1, NULL);
+        }
+        else {
+            ReleaseSemaphore(semaphore2, 1, NULL);
+        }
     }
 }
 
-void Read() {
+void Read(int proccess_number) {
     char buffer[256] = { 0 };
     std::ifstream in;
     while (true) {
-        WaitForSingleObject(semaphore1, INFINITE);
+        if (proccess_number == 1) {
+            WaitForSingleObject(semaphore2, INFINITE);
+        }
+        else {
+            WaitForSingleObject(semaphore1, INFINITE);
+        }
 
         in.open("msg");
-
-        in >> buffer;
+        in.getline(buffer, 256);
         std::cout << "\n > got message: " << buffer << "\n >> ";
-
         in.close();
     }
 }
 
-int main() {
-    semaphore1 = CreateSemaphore(NULL, 0, 1, L"Semaphore");
-    std::thread WriteThread(Write);
-    std::thread ReadThread(Read);
+int main(int argc, char *argv[]) {
+    assert(argc == 2);
+
+    int proccess_number = atoi(argv[1]);
+    assert(proccess_number == 1 || proccess_number == 2);
+
+    semaphore1 = CreateSemaphore(NULL, 0, 1, L"S1");
+    semaphore2 = CreateSemaphore(NULL, 0, 1, L"S2");
+
+    std::thread WriteThread(Write, proccess_number);
+    std::thread ReadThread(Read, proccess_number);
 
     WriteThread.join();
     ReadThread.join();

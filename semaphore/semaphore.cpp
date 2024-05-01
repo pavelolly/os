74de3871c65f6218a159d6a28d38d64386c16648
km <lvanfomin069@gmail.com>
2024-05-01 20:10:26 +0300

    add three different ways to share memory between processes

--- semaphore/semaphore.cpp
+++ semaphore/semaphore.cpp
@@ -8,6 +8,8 @@
 HANDLE semaphore1;
 HANDLE semaphore2;
 
+namespace task1 {
+
 void Write(int proccess_number) {
     char buffer[256];
     std::ofstream out;
@@ -46,6 +48,88 @@ void Read(int proccess_number) {
     }
 }
 
+}
+
+HANDLE hFile;
+HANDLE hMapFile;
+char* map_view;
+
+namespace task2 {
+
+void Write(int proccess_number) {
+    char buffer[256];
+
+	while (true) {
+		std::cout << " >> ";
+		std::cin.getline(buffer, 256);
+
+        strcpy_s(map_view, 256, buffer);
+
+		if (proccess_number == 1) {
+			ReleaseSemaphore(semaphore1, 1, NULL);
+		}
+		else {
+			ReleaseSemaphore(semaphore2, 1, NULL);
+		}
+	}
+}
+
+void Read(int proccess_number) {
+	char buffer[256];
+
+	while (true) {
+		if (proccess_number == 1) {
+			WaitForSingleObject(semaphore2, INFINITE);
+		}
+		else {
+			WaitForSingleObject(semaphore1, INFINITE);
+		}
+
+		std::cout << "\n > got message: " << map_view << "\n >> ";
+	}
+}
+
+}
+
+char* shared_memory;
+
+namespace task3 {
+
+void Write(int proccess_number) {
+	char buffer[256];
+
+	while (true) {
+		std::cout << " >> ";
+		std::cin.getline(buffer, 256);
+
+		strcpy_s(map_view, 256, buffer);
+
+		if (proccess_number == 1) {
+			ReleaseSemaphore(semaphore1, 1, NULL);
+		}
+		else {
+			ReleaseSemaphore(semaphore2, 1, NULL);
+		}
+	}
+}
+
+void Read(int proccess_number) {
+	char buffer[256];
+
+	while (true) {
+		if (proccess_number == 1) {
+			WaitForSingleObject(semaphore2, INFINITE);
+		}
+		else {
+			WaitForSingleObject(semaphore1, INFINITE);
+		}
+
+		std::cout << "\n > got message: " << map_view << "\n >> ";
+	}
+}
+
+}
+
 PROCESS_INFORMATION runUrself(const wchar_t *_cmd) {
     STARTUPINFO startupinfo;
     ZeroMemory(&startupinfo, sizeof(startupinfo));
@@ -74,13 +158,39 @@ int main(int argc, char *argv[]) {
 
         WaitForSingleObject(process1.hProcess, INFINITE);
         WaitForSingleObject(process2.hProcess, INFINITE);
+
+        CloseHandle(process1.hProcess);
+        CloseHandle(process2.hProcess);
     }
     else {
         int proccess_number = atoi(argv[1]);
         assert(proccess_number == 1 || proccess_number == 2);
 
+        std::cout << "PROCESS: " << proccess_number << std::endl;
+
+        // task 1, 2, 3
         semaphore1 = CreateSemaphore(NULL, 0, 1, L"S1");
         semaphore2 = CreateSemaphore(NULL, 0, 1, L"S2");
+
+        // task 2
+        hFile = CreateFileW(L"msg", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+        assert(hFile != INVALID_HANDLE_VALUE);
+
+        hMapFile = CreateFileMappingW(hFile, NULL, PAGE_READWRITE, 0, 256, L"Map");
+        assert(hMapFile);
+
+        map_view = (char*)MapViewOfFile(hMapFile, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+        assert(map_view);
+
+        // task3
+		HANDLE shared_memory_handle = CreateFileMappingW(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 256, L"Shared memmory");
+        assert(shared_memory_handle);
+
+        // Получение указателя на разделяемую память
+        shared_memory = (char*)MapViewOfFile(shared_memory_handle, FILE_MAP_ALL_ACCESS, 0, 0, 0);    
+        assert(shared_memory);
+        
+        using namespace task3;
 
         std::thread WriteThread(Write, proccess_number);
         std::thread ReadThread(Read, proccess_number);

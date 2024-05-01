7d47a2b00288f3782f84af2f66122d6ab63f8231
km <lvanfomin069@gmail.com>
2024-05-01 19:00:26 +0300

    parent process now summon two "user" processes

--- semaphore/semaphore.cpp
+++ semaphore/semaphore.cpp
@@ -46,20 +46,48 @@ void Read(int proccess_number) {
     }
 }
 
+PROCESS_INFORMATION runUrself(const wchar_t *_cmd) {
+    STARTUPINFO startupinfo;
+    ZeroMemory(&startupinfo, sizeof(startupinfo));
+    startupinfo.cb = sizeof(startupinfo);
+
+    PROCESS_INFORMATION processinfo;
+    ZeroMemory(&processinfo, sizeof(processinfo));
+
+    wchar_t cmd[256];
+    wcscpy_s(cmd, _cmd);
+
+    bool success = CreateProcessW(L"C:\\Users\\km\\source\\repos\\pavelolly\\os\\semaphore\\x64\\Debug\\semaphore.exe", cmd,
+        NULL, NULL, false, CREATE_NEW_CONSOLE, NULL, NULL, &startupinfo, &processinfo);
+
+    assert(success);
+
+    return processinfo;
+}
+
 int main(int argc, char *argv[]) {
-    assert(argc == 2);
+    assert(argc <= 2);
 
-    int proccess_number = atoi(argv[1]);
-    assert(proccess_number == 1 || proccess_number == 2);
+    if (argc == 1) {
+        PROCESS_INFORMATION process1 = runUrself(L"semaphore.exe 1");
+        PROCESS_INFORMATION process2 = runUrself(L"semaphore.exe 2");
 
-    semaphore1 = CreateSemaphore(NULL, 0, 1, L"S1");
-    semaphore2 = CreateSemaphore(NULL, 0, 1, L"S2");
+        WaitForSingleObject(process1.hProcess, INFINITE);
+        WaitForSingleObject(process2.hProcess, INFINITE);
+    }
+    else {
+        int proccess_number = atoi(argv[1]);
+        assert(proccess_number == 1 || proccess_number == 2);
 
-    std::thread WriteThread(Write, proccess_number);
-    std::thread ReadThread(Read, proccess_number);
+        semaphore1 = CreateSemaphore(NULL, 0, 1, L"S1");
+        semaphore2 = CreateSemaphore(NULL, 0, 1, L"S2");
 
-    WriteThread.join();
-    ReadThread.join();
+        std::thread WriteThread(Write, proccess_number);
+        std::thread ReadThread(Read, proccess_number);
+
+        WriteThread.join();
+        ReadThread.join();
+    }
 
     return 0;
 }
